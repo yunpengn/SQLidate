019e06e8cd441cdc87a58983b895a747b3416c95
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-01-02 00:09:34 +0800

    Create connection to db

--- src/main/java/com/yunpengn/Main.java
+++ src/main/java/com/yunpengn/Main.java
@@ -1,7 +1,27 @@
 package com.yunpengn;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ * The main driver class.
+ */
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
+  private static final String PROPERTY_FILE_NAME = "config.properties";
+
+  public static void main(String[] args) throws Exception {
+    Connection connection = createConnection();
+  }
+
+  private static Connection createConnection() throws Exception {
+    final Properties props = new Properties();
+    props.load(new FileInputStream(PROPERTY_FILE_NAME));
+
+    String dbName = props.getProperty("db");
+    String url = "jdbc:postgresql://localhost/" + dbName;
+    return DriverManager.getConnection(url, props);
   }
 }
