183fe6dc685708ac4ce4377e2f3752cca37498ec
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-03-14 15:01:54 +0800

    Add script

--- scripts/run.sh
+++ scripts/run.sh
@@ -20,10 +20,25 @@ generate() {
   psql random -f insert_data_small.sql
 }
 
+generateBig() {
+  cd "$(dirname "$0")"
+  psql random -f create_db_big.sql
+
+  cd tables/
+  psql random -f a.sql
+  psql random -f b.sql
+  psql random -f c.sql
+  psql random -f d.sql
+  psql random -f e.sql
+  psql random -f f.sql
+}
+
 if [[ "$1" == "load" ]]; then
   load_data
 elif [[ "$1" == "generate" ]]; then
   generate
+elif [[ "$1" == "generate-big" ]]; then
+  generateBig
 elif [[ "$1" == "check" ]]; then
   check_query
 else
